23051a455afdddd68af1e32c67107c5eec7f6636
thakkar <tayebhakkar@yahoo.fr>
2024-02-10 18:26:29 +0100

    Dijkstra with working checkpoints
    For now, the path is not the optimal. Indeed it take the order of placement of the checkpoint and not the optimal order. Then, the path between two point is optimal, but not necessarily the complete path.
    
    Maybe an upgrade would be :
    Allowing the user to choose which method to use. Either by creating a check box or a new instance of algorithm

--- scripts/board.js
+++ scripts/board.js
@@ -1,8 +1,21 @@
+export const CELLS_TYPES = {
+    EMPTY: 0,
+    WALL: 1,
+    START: 2,
+    END: 3,
+    CHECKPOINT: 4
+}
+    
+
 export class Board{
     constructor(rows,cols){
+        this.webElement = document.getElementsByClassName('board')[0];
         this.cols = cols;
         this.rows = rows;
         this.graph = [];
+        this.startPoint = {x: 0, y: 0};
+        this.endPoints = [];
+        this.checkpoints = [];
     }
 
     initialise(){
@@ -13,10 +26,128 @@ export class Board{
         var table = '';
         for(var r = 0; r < this.rows; ++r){
             table += `<tr id="${'row'+r}">`;
+            this.graph[r] = [];
             for(var c = 0; c < this.cols; ++c){
-                table += `<td id="${'row'+r+'cell'+c}"></td>`;
+                this.graph[r][c] = CELLS_TYPES.EMPTY;
+                table += `<td id="${r+';'+c}"></td>`;
             }
         }
-        document.getElementsByClassName('board')[0].innerHTML = table;
+        this.webElement.innerHTML = table;
+    }
+
+    checkCell(x,y, type){
+        if(type == CELLS_TYPES.START){
+            if(this.startPoint.x != undefined && this.startPoint.y != undefined){
+                this.uncheckCell(this.startPoint.x, this.startPoint.y);
+            }
+            this.graph[x][y] = type;
+            this.startPoint.x = x;
+            this.startPoint.y = y;
+            return;
+        }
+        if(type == CELLS_TYPES.END){
+            this.graph[x][y] = type;
+            this.endPoints.push({x: x, y: y});
+            return;
+        }
+        if(type == CELLS_TYPES.CHECKPOINT){
+            if(this.graph[x][y] == CELLS_TYPES.CHECKPOINT){
+                return;
+            }
+            this.graph[x][y] = type;
+            this.checkpoints.push({x: x, y: y});
+            document.getElementById(x+';'+y).textContent = this.checkpoints.length;
+            return;
+        }
+        
+        this.graph[x][y] = type;
+    }
+
+    uncheckCell(x,y){
+        console.log(x,y);
+        if(this.graph[x][y] == CELLS_TYPES.START){
+            this.startPoint = {};
+        }
+        if(this.graph[x][y] == CELLS_TYPES.END){
+            this.endPoints = this.endPoints.filter(end => end.x != x || end.y != y);
+        }
+        if(this.graph[x][y] == CELLS_TYPES.CHECKPOINT){
+            this.checkpoints = this.checkpoints.filter(checkpoint => checkpoint.x != x || checkpoint.y != y);
+        }
+        this.graph[x][y] = CELLS_TYPES.EMPTY;
+        document.getElementById(x+';'+y).classList = [];
+    }   
+
+    clear(){
+        for(var r = 0; r < this.rows; ++r){
+            for(var c = 0; c < this.cols; ++c){
+                this.graph[r][c] = CELLS_TYPES.EMPTY;
+                document.getElementById(r+';'+c).classList = [];
+                document.getElementById(r+';'+c).textContent = '';
+            }
+        }
+        this.startPoint = {};
+        this.endPoints = [];
+        this.checkpoints = [];
+    }
+
+    clearPath(){
+        for(var r = 0; r < this.rows; ++r){
+            for(var c = 0; c < this.cols; ++c){
+                document.getElementById(r+';'+c).classList.remove('path');
+            }
+        }
+    }
+
+    clearVisited(){
+        for(var r = 0; r < this.rows; ++r){
+            for(var c = 0; c < this.cols; ++c){
+                document.getElementById(r+';'+c).classList.remove('visited');
+            }
+        }
+    }
+
+    toAdjacencyMatrix(){
+        let adjacencyMatrix = [];
+
+        // Convert the graph to an adjacency matrix
+        for(var r = 0; r < this.rows; ++r){
+            adjacencyMatrix[r] = [];
+            for(var c = 0; c < this.cols; ++c){
+                adjacencyMatrix[r][c] = this.graph[r][c] == CELLS_TYPES.WALL ? 0 : 1;
+            }
+        }
+
+        return adjacencyMatrix;
+    }
+
+    toAdjacencyList(){
+        let adjacencyList = {};
+
+        // Convert the graph to an adjacency list
+        for(var r = 0; r < this.rows; ++r){
+            for(var c = 0; c < this.cols; ++c){
+                if(this.graph[r][c] == CELLS_TYPES.WALL){
+                    continue;
+                }
+
+                let key = r+';'+c;
+                adjacencyList[key] = [];
+                if(r > 0 && this.graph[r-1][c] != CELLS_TYPES.WALL){
+                    adjacencyList[key].push((r-1)+';'+c);
+                }
+                if(c > 0 && this.graph[r][c-1] != CELLS_TYPES.WALL){
+                    adjacencyList[key].push(r+';'+(c-1));
+                }
+                if(r < this.rows-1 && this.graph[r+1][c] != CELLS_TYPES.WALL){
+                    adjacencyList[key].push((r+1)+';'+c);
+                }
+                if(c < this.cols-1 && this.graph[r][c+1] != CELLS_TYPES.WALL){
+                    adjacencyList[key].push(r+';'+(c+1));
+                }
+            }
+        }
+
+        return adjacencyList;
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,186 @@
-import { Board } from "./board.js";
+import { AlgorithmFactory } from "./algorithm.js";
+import { Board, CELLS_TYPES } from "./board.js";
 
 document.addEventListener("DOMContentLoaded",function(){
-    var nav = document.getElementsByTagName("NAV")[0];
-
-    nav.addEventListener('click',function(e){
-        console.log(e.target)
-        var lastActive = document.getElementsByClassName('navActive')[0];
-        var newActive = e.target.closest('li');
-        if(lastActive != undefined){
-            lastActive.classList.remove('navActive');
-        }
-        if(newActive != undefined && newActive != null && newActive.id != 'item0' && newActive.id != 'item5'){
-            newActive.classList.add('navActive');
-        }
-    })
-    var width = document.getElementsByClassName('board')[0].clientWidth;
-    var height = document.getElementsByClassName('board')[0].clientHeight;
+    let nav = document.getElementsByTagName("NAV")[0];
+    let sideMenu = document.getElementsByClassName("sideMenu")[0];
+    let width = document.getElementsByClassName('board')[0].clientWidth;
+    let height = document.getElementsByClassName('board')[0].clientHeight;
     
-    var rows = Math.floor(height / 25);
-    var cols = Math.floor(width / 25);
-    var board = new Board(rows,cols);
+    let rows = Math.floor(height / 25);
+    let cols = Math.floor(width / 25);
+    let board = new Board(rows,cols);
     board.createGride();
+    
+    let mouseDown = false;
+    let mouseButton = -1;
+
+    let algoIsRunning = false;
+    let cellType = CELLS_TYPES.WALL;
+
+    const SPEEDS = {
+        1: 100,
+        2: 50,
+        4: 25,
+        8: 10,
+    }
+
+    let currentSpeed = 1;
+
+    document.addEventListener('mousedown',function(e){
+        if(algoIsRunning){
+            return;
+        }
+        mouseDown = true;
+        mouseButton = e.button;
+    });
+
+    document.addEventListener('mouseup',function(e){
+        if(algoIsRunning){
+            return;
+        }
+        mouseDown = false;
+        mouseButton = -1;
+    });
+
+    document.addEventListener('mouseover',function(e){
+        if(algoIsRunning){
+            return;
+        }
+        if(mouseDown){
+            if(e.target.tagName == 'TD'){
+                clickOnBoardHandler(e);
+            }
+        }
+    });
+
+    document.addEventListener('contextmenu', function(e){
+        e.preventDefault();
+        // Board
+        if(e.target.tagName == 'TD'){
+            mouseButton = e.button;
+            clickOnBoardHandler(e);
+        }
+    });
+
+    document.addEventListener('click',function(e){
+        if(algoIsRunning){
+            return;
+        }
+        // Navbar
+        if(e.target.closest('nav')){
+            clickOnNavHandler(e);
+        }
+
+        // Board
+        if(e.target.tagName == 'TD'){
+            mouseButton = e.button;
+            clickOnBoardHandler(e);
+        }
+
+        // Side Menu toggle
+        if(e.target.classList.contains('sideMenu__toggle')){
+            clickOnSideMenuToggleHandler(e);
+        }
+    });
+
+    const clickOnBoardHandler = (e) => {
+        const [x,y] = e.target.id.split(';');
+        if(mouseButton == 0){
+            e.target.classList = [];
+
+            switch(cellType){
+                case CELLS_TYPES.EMPTY:
+                    return;
+                case CELLS_TYPES.WALL:
+                    e.target.classList.add('wall');
+                    break;
+                case CELLS_TYPES.START:
+                    e.target.classList.add('start');
+                    break;
+                case CELLS_TYPES.END:
+                    e.target.classList.add('end');
+                    break;
+                case CELLS_TYPES.CHECKPOINT:
+                    e.target.classList.add('checkpoint');
+                    break;
+                default:
+                    return;
+            }
+
+            board.checkCell(x,y,cellType);
+        }else{
+            e.target.classList.remove('wall');
+            board.uncheckCell(x,y);
+        }
+    }
+
+    const clickOnNavHandler = async (e) => {
+        if(e.target.tagName == 'INPUT'){
+            cellType = parseInt(e.target.value);
+            return;
+        }
+        if(e.target.tagName == 'BUTTON'){
+            // TODO
+        }
+        if(e.target.tagName == 'A' || e.target.tagName == 'BUTTON' || e.target.tagName == 'SPAN'){
+            let item = e.target.closest('li');
+            if(!item){
+                return;
+            }
+            switch(item.id){
+                case 'item0':
+                    // Nothing to do
+                    break;
+                case 'item1':
+                    // TODO (submenu for algorithm selection)
+                    break;
+                case 'item2':
+                    // TODO (submenu for maze generation)
+                    break;
+                case 'item3':
+                    currentSpeed = currentSpeed * 2;
+                    if(currentSpeed > 8){
+                        currentSpeed = 1;
+                    }
+                    item.getElementsByTagName('span')[0].innerText = `x${currentSpeed}`
+                    break;
+                case 'item4':
+                    board.clear();
+                    break;
+                case 'item5':
+                    // TODO : clean alert
+                    if(board.startPoint.x == undefined || board.endPoints.length == 0){
+                        alert("You must select a start point and at least one end point!");
+                        return;
+                    }
+                    board.clearVisited();
+                    board.clearPath();
+                    algoIsRunning = true;
+                    let path = await AlgorithmFactory.create("Dijkstra", board).run(SPEEDS[currentSpeed]);
+                    algoIsRunning = false;
+                    if(path && path.length > 1){
+                        for(let cell of path){
+                            await new Promise(resolve => setTimeout(resolve, SPEEDS[currentSpeed]));
+
+                            if(cell != board.startPoint.x+';'+board.startPoint.y && board.endPoints.map(end => end.x+';'+end.y).indexOf(cell) == -1 && document.getElementById(cell).classList.contains('visited')){
+                                document.getElementById(cell).classList = [];
+                                document.getElementById(cell).classList.add('path');
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+    
+    const clickOnSideMenuToggleHandler = (e) => {
+        if(sideMenu.classList.contains('sideMenu--hidden')){
+            sideMenu.classList.remove('sideMenu--hidden');
+            e.target.classList.add('sideMenu__toggle--rotated');
+        }else{
+            sideMenu.classList.add('sideMenu--hidden');
+            e.target.classList.remove('sideMenu__toggle--rotated');
+        }
+    }
 });
